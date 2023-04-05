e0fbafe708bdb903d7062705b4dfa2d5571429f9
Silas Lima e Silva <silaslima@usp.br>
2023-04-04 22:06:58 -0300

    adding other urls

--- src/core/base-urls.ts
+++ src/core/base-urls.ts
@@ -1,4 +1,11 @@
+export const POSITIONS_BASE_URL =
+  'https://positions-run-service-mgz5b2qjuq-rj.a.run.app/';
+
+export const FEEDBACK_BASE_URL =
+  'https://feedback-run-service-mgz5b2qjuq-rj.a.run.app/';
+
 export const STUDENTS_BASE_URL =
   "https://students-run-service-mgz5b2qjuq-rj.a.run.app/";
+
 export const AUTH_BASE_URL = 
   "https://auth-run-service-mgz5b2qjuq-rj.a.run.app/";
